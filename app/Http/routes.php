ab2bc8c920d669393898402ef73f449b86d686ba
unknown <hasankhan.cse@gmail.com>
2019-03-07 18:15:46 +0600

    jQuery pactice

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,8 +1,8 @@
 <?php
-/*
+
 Route::get('/', function () {
     return view('welcome');
 });
-*/
+
 Route::get('index',"crudController@index");
 Route::get('jquery',"crudController@getJquery");
--- resources/views/jquery.blade.php
+++ resources/views/jquery.blade.php
@@ -1,8 +1,9 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 	<style type="text/css">
-		#panel, #flip {
+		/* #panel, #flip {
 		  padding: 5px;
 		  font-size: 18px;
 		  text-align: center;
@@ -11,15 +12,32 @@
 		  border: solid 1px #666;
 		  border-radius: 3px;
 		}
-
+		
 		#panel {
 		  padding: 50px;
 		  display: none;
+		} */
+
+		table {
+		  font-family: arial, sans-serif;
+		  border-collapse: collapse;
+		  width: 100%;
 		}
+
+		td, th {
+		  border: 1px solid #dddddd;
+		  text-align: left;
+		  padding: 8px;
+		}
+
+		tr:nth-child(even) {
+		  background-color: #dddddd;
+		}
+
 	</style>
 </head>
 <body>
-
+<!-- 
 <p id="para">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus beatae optio neque soluta distinctio, perferendis quibusdam ex, est doloribus quos quis deleniti. Soluta explicabo, illo aut odio, quae blanditiis sed!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae quaerat itaque provident minus pariatur, omnis exercitationem vero sunt molestias explicabo rerum esse distinctio aliquid. Quidem animi pariatur deleniti, deserunt provident!!!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus beatae optio neque soluta distinctio, perferendis quibusdam ex, est doloribus quos quis deleniti. Soluta explicabo, illo aut odio, quae blanditiis sed!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae quaerat itaque provident minus pariatur, omnis exercitationem vero sunt molestias explicabo rerum esse distinctio aliquid. Quidem animi pariatur deleniti, deserunt provident!!!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus beatae optio neque soluta distinctio, perferendis quibusdam ex, est doloribus quos quis deleniti. Soluta explicabo, illo aut odio, quae blanditiis sed!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae quaerat itaque provident minus pariatur, omnis exercitationem vero sunt molestias explicabo rerum esse distinctio aliquid. Quidem animi pariatur deleniti, deserunt provident!!! aliquid. Quidem animi pariatur deleniti, deserunt provident!!!erunt provident!!!</p>
 <button id="Chaining">Click me</button><br><br><br>
 
@@ -33,10 +51,107 @@
 <button>Click to fade in boxes</button><br><br>
 <p id="div1" style="width:80px;height:80px;display:none;color:red;">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus voluptate reiciendis nemo fugiat nihil in officiis ipsa, delectus nisi ratione. Suscipit, modi officiis, autem porro illum reiciendis nisi. Doloremque, explicabo.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt nobis, illum tempore at voluptatibus maxime exercitationem, odio asperiores aspernatur error ipsum impedit magnam accusamus rem minima sequi, porro! Exercitationem, adipisci.</p><br>
 
+ -->
+<!-- <p>This is a paragraph.</p>
+<button>Test jQuery</button> -->
+
+
+<!-- <input id="myInput" type="text" placeholder="Search..">
+<br><br>
+
+<table>
+  <thead>
+  <tr>
+    <th>Firstname</th>
+    <th>Lastname</th>
+    <th>Email</th>
+  </tr>
+  </thead>
+  <tbody id="myTable">
+  <tr>
+    <td>Hasan</td>
+    <td>Mahmud</td>
+    <td>hasan@example.com</td>
+  </tr>
+  <tr>
+    <td>Mary</td>
+    <td>Moe</td>
+    <td>mary@mail.com</td>
+  </tr>
+  <tr>
+    <td>July</td>
+    <td>Dooley</td>
+    <td>july@greatstuff.com</td>
+  </tr>
+  <tr>
+    <td>Anja</td>
+    <td>Ravendale</td>
+    <td>a_r@test.com</td>
+  </tr>
+  </tbody>
+</table> -->
+<!-- <div id="div1"><h2>Let jQuery AJAX Change This Text</h2></div>
+<button>Get External Content</button><br><br>
+
+
+<form action="">
+  First name: <input type="text" name="FirstName"><br>
+  Last name: <input type="text" name="LastName"><br>
+</form>
+
+<input type="button" value='Serialize form values'/>
+<div id="results"></div> -->
+
+<ol>
+  <li>Append item</li>
+  <li>Append item</li>
+</ol>
+<button id="btn2">Append list items</button>
+<button id="btn">Prepend list items</button><br><br>
+
+<button id="click">Remove the div element</button>
+<div id="empty" style="height:100px;width:300px;border:1px solid black;background-color:yellow;">
+	This is some text in the div.
+<p>This is a paragraph in the div.</p>
+<p>This is another paragraph in the div.</p>
+</div>
+
+
+
+<input type="text" id="jACls">
+
+
+<h3 id="cssName">Hasan Mahmud</h3>
+<p id="mulcss">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui, molestiae. Dicta placeat consequuntur non tempora pariatur voluptatem tenetur, quo incidunt, voluptates earum velit dolor, doloremque delectus enim dolorum molestias nesciunt.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente quam, debitis pariatur necessitatibus animi perferendis ab, dolore veniam consequatur! Accusantium reiciendis, sunt eveniet nisi voluptas ducimus obcaecati numquam natus cupiditate.</p>
+
+<!-- <button>Empty div element</button><br><br>
+<div id="div1" style="height:100px;width:300px;border:1px solid black;background-color:yellow;">-->
+
+
+<button id="ajaxGet">Send an HTTP GET request to a page and get the result back</button>
+
+<button id="ajaxPost">Send an HTTP POST request to a page and get the result back</button>
+
+
+<div id="ajaxload"><h2>Let jQuery AJAX Change This Text</h2></div>
+<button id="ajaxload1">Get External Content</button><br><br><br>
+
+
+<input id="myInputFilter" type="text" placeholder="Search..">
+<br>
+
+<ul id="myTextList">
+  <li>First item</li>
+  <li>Second item</li>
+  <li>Third item</li>
+  <li>Fourth</li>
+</ul>
+
+<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <script>
 $(document).ready(function(){
-  $("#Chaining").click(function(){
+  /*$("#Chaining").click(function(){
     $("#para").css("color", "red").slideUp(2000).slideDown(2000);
   });
 
@@ -56,9 +171,113 @@ $(document).ready(function(){
 
   $("button").click(function(){
     $("#div1").fadeIn(3000);
-  });
+  });*/
+
+  	/*$.noConflict();
+	jQuery(document).ready(function(){
+	  jQuery("button").click(function(){
+	    jQuery("p").text("jQuery is still working!");
+	  });
+	});*/
+
+
+  /*$("#myInput").on("keyup", function() {
+    var value = $(this).val().toLowerCase();
+    $("#myTable tr").filter(function() {
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+    });
+  });*/
 
 });
+
+
+/*$(document).ready(function(){
+  $("button").click(function(){
+    $.ajax({url: "/", success: function(result){
+      $("#div1").html(result);
+    }});
+  });
+
+
+  $("input").click(function(){
+    var x = $("form").serializeArray();
+    $.each(x, function(i, field){
+      $("#results").append(field.name + ":" + field.value + " ");
+    });
+  });
+});*/
+
+
+$(document).ready(function(){
+	$("#jACls").addClass("form-control");
+});
+
+
+
+$(document).ready(function(){
+  $("#btn2").click(function(){
+    $("ol").append("<li> append item</li>");
+  });
+});
+
+ $("#btn").click(function(){
+    $("ol").prepend("<li>Prepend item</li>");
+  });
+
+/*
+ $("button").click(function(){
+    $("#div1").remove();
+  });*/
+
+  $("#click").click(function(){
+    $("#empty").empty();
+  });
+
+
+  $(document).ready(function(){
+  	$("#cssName").css("background-color", "yellow");
+  	$("#mulcss").css({"background-color": "yellow", "font-size": "200%"});
+  });
+
+
+
+   $("#ajaxGet").click(function(){
+    $.get("/", function(data, status){
+      // alert("Data: " + data + "\nStatus: " + status);
+      console.log(data);
+    });
+  });
+
+
+  $(document).ready(function(){
+	  $("#ajaxPost").click(function(){
+	    $.post("/",
+	    {
+	      name: "Donald Duck",
+	      city: "Duckburg"
+	    },
+	    function(data,status){
+	      alert("Data: " + data + "\nStatus: " + status);
+	    });
+	  });
+  });
+
+	$(document).ready(function(){
+	  $("#ajaxload1").click(function(){
+	    $("#ajaxload").load('{{URL::to("/")}}');
+	  });
+	});
+
+
+	$(document).ready(function(){
+	  $("#myInputFilter").on("keyup", function() {
+	    var value = $(this).val().toLowerCase();
+	    $("#myTextList li").filter(function() {
+	      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+	    });
+	  });
+	});
+
 </script>
 </body>
 </html>
