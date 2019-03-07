8859aa4e88f53e18696095db14a14870adb2b031
unknown <hasankhan.cse@gmail.com>
2019-03-07 12:07:07 +0600

    jQury two version use

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,59 +4,44 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+	<script
+  src="https://code.jquery.com/jquery-1.5.min.js"
+  integrity="sha256-IpJ49qnBwn/FW+xQ8GVI/mTCYp9Z9GLVDKwo5lu5OoM="
+  crossorigin="anonymous"></script>
+  <script type="text/javascript">
+  	   var jQ5 = $.noConflict(true); 
+  </script>
+
+  <script
+  src="https://code.jquery.com/jquery-2.2.0.min.js"
+  integrity="sha256-ihAoc6M/JPfrIiIeayPE9xjin4UWjsx2mjW/rtmxLM4="
+  crossorigin="anonymous"></script>
+  <script type="text/javascript">
+  	   var jQ22 = $.noConflict(true);
+  </script>	
+
+  <script type="text/javascript">
+  	  jQ5(document).ready(function(){
+  	  	jQ5("#jQ5").live("click", function(){
+  	  		alert('This is jQuery older version 1.5.0')
+  	  	});
+  	  });
+
+  	  jQ22(document).ready(function(){
+  	  	jQ22("#jQ22").on("click", function(){
+  	  		alert('This is jQuery new version 2.2.0');
+  	  	});
+  	  });
+  </script>
 
     <title>First Page!</title>
   </head>
   <body>
     <div class="container">
     	<div class="row">
-    		<h1>Laravel Framework!</h1>
-		    <form>
-			  <div class="form-row">
-			    <div class="form-group col-md-6">
-			      <label for="inputEmail4">Email</label>
-			      <input type="email" class="form-control" id="inputEmail4" placeholder="Email">
-			    </div>
-			    <div class="form-group col-md-6">
-			      <label for="inputPassword4">Password</label>
-			      <input type="password" class="form-control" id="inputPassword4" placeholder="Password">
-			    </div>
-			  </div>
-			  <div class="form-group">
-			    <label for="inputAddress">Address</label>
-			    <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
-			  </div>
-			  <div class="form-group">
-			    <label for="inputAddress2">Address 2</label>
-			    <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
-			  </div>
-			  <div class="form-row">
-			    <div class="form-group col-md-6">
-			      <label for="inputCity">City</label>
-			      <input type="text" class="form-control" id="inputCity">
-			    </div>
-			    <div class="form-group col-md-4">
-			      <label for="inputState">State</label>
-			      <select id="inputState" class="form-control">
-			        <option selected>Choose...</option>
-			        <option>...</option>
-			      </select>
-			    </div>
-			    <div class="form-group col-md-2">
-			      <label for="inputZip">Zip</label>
-			      <input type="text" class="form-control" id="inputZip">
-			    </div>
-			  </div>
-			  <div class="form-group">
-			    <div class="form-check">
-			      <input class="form-check-input" type="checkbox" id="gridCheck">
-			      <label class="form-check-label" for="gridCheck">
-			        Check me out
-			      </label>
-			    </div>
-			  </div>
-			  <button type="submit" class="btn btn-primary">Sign in</button>
-			</form>
+    		<h1>How to use jQuery Two version!</h1>
+		    <button id="jQ5">jQuery5</button>
+		    <button id="jQ22">jQuery2.2.0</button>
     	</div>
     </div>
 
