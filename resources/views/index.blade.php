e581e2aa60a5ed52a096e5b028a5acee751888b5
unknown <hasankhan.cse@gmail.com>
2019-03-07 15:59:04 +0600

    jQ event

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,35 +4,22 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <style> 
-		#panel, #flip {
-		  padding: 5px;
-		  text-align: center;
-		  background-color: #e5eecc;
-		  border: solid 1px #c3c3c3;
-		}
-
-		#panel {
-		  padding: 50px;
-		  display: none;
-		}
-	</style>
 	<script
-  src="https://code.jquery.com/jquery-1.5.min.js"
-  integrity="sha256-IpJ49qnBwn/FW+xQ8GVI/mTCYp9Z9GLVDKwo5lu5OoM="
-  crossorigin="anonymous"></script>
-  <script type="text/javascript">
-  	   var jQ5 = $.noConflict(true); 
-  </script>
-
-  <script
-  src="https://code.jquery.com/jquery-2.2.0.min.js"
-  integrity="sha256-ihAoc6M/JPfrIiIeayPE9xjin4UWjsx2mjW/rtmxLM4="
-  crossorigin="anonymous"></script>
-  <script type="text/javascript">
-  	   var jQ22 = $.noConflict(true);
-  </script>	
-
+	  src="https://code.jquery.com/jquery-1.5.min.js"
+	  integrity="sha256-IpJ49qnBwn/FW+xQ8GVI/mTCYp9Z9GLVDKwo5lu5OoM="
+	  crossorigin="anonymous"></script>
+	  <script type="text/javascript">
+	  	   var jQ5 = $.noConflict(true); 
+	  </script>
+	
+	  <script
+	  src="https://code.jquery.com/jquery-2.2.0.min.js"
+	  integrity="sha256-ihAoc6M/JPfrIiIeayPE9xjin4UWjsx2mjW/rtmxLM4="
+	  crossorigin="anonymous"></script>
+	  <script type="text/javascript">
+	  	   var jQ22 = $.noConflict(true);
+	  </script>	
+	 
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script type="text/javascript">
   	   var jQ3 = $.noConflict(true);
@@ -108,9 +95,7 @@
 
 		 $("#show").click(function(){
 		    $("#hideShow").show();
-		 });
-
-		
+		 });			
   	  });
   </script>
 	
@@ -120,26 +105,21 @@
   <body>
     <div class="container">
 		<h1 class="text-center">How to use jQuery all version!</h1><br><br>
-			    <button class="btn btn-info btn-sm" id="jQ5">jQuery5 click event</button><br><br>
-			    <button class="btn btn-primary btn-sm" id="jQ22">jQuery2.2.0 click event</button><br><br>
-			    <button class="btn btn-warning btn-sm" id="clickEventHide">This is click event text hide</button><br><br>
-			    <button class="btn btn-success btn-sm" id="dblClickHideEvent">This is bouble click event text hide</button><br><br>
-			    <button class="btn btn-success btn-sm" id="mouseEnter">This is mouseenter event</button><br><br>
-			    <button class="btn btn-info btn-sm" id="mouseleave">This is mouseleave event</button><br><br>
-			    <button class="btn btn-info btn-sm" id="mouseup">This is mouseup event</button><br><br>
-			    <button class="btn btn-dark btn-sm" id="mousedown">This is mousedown event</button><br><br>
-			    <button class="btn btn-primary btn-sm" id="hover">This is hover event</button><br><br>
-			    <input id="fullname" type="text" name="fullname">
-			    <input id="email" type="text" name="email">
-		<h4 id="name">Hasan Mahmud khan.</h4>
-		<h5 id="hideShow">Welcome to jQuery</h5>
-		<button id="hide">Hide</button>
-		<button id="show">Show</button>
-		
+	   <button class="btn btn-info btn-sm" id="jQ5">jQuery5 click event</button><br><br>
+	   <button class="btn btn-primary btn-sm" id="jQ22">jQuery2.2.0 click event</button><br><br>
+	   <button class="btn btn-warning btn-sm" id="clickEventHide">This is click event text hide</button><br><br>
+	   <button class="btn btn-success btn-sm" id="dblClickHideEvent">This is bouble click event text hide</button><br><br>
+	   <button class="btn btn-success btn-sm" id="mouseEnter">This is mouseenter event</button><br><br>
+	   <button class="btn btn-info btn-sm" id="mouseleave">This is mouseleave event</button><br><br>
+	   <button class="btn btn-info btn-sm" id="mouseup">This is mouseup event</button><br><br>
+	   <button class="btn btn-dark btn-sm" id="mousedown">This is mousedown event</button><br><br>
+	   <button class="btn btn-primary btn-sm" id="hover">This is hover event</button><br><br>
+	   <input id="fullname" type="text" name="fullname">
+	   <input id="email" type="text" name="email">
+	   		<h4 id="name">Hasan Mahmud khan.</h4>
+	   		<h5 id="hideShow">Welcome to jQuery</h5>
+	   		<button id="hide">Hide</button>
+	   		<button id="show">Show</button>
     </div>
-
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
   </body>
 </html>
